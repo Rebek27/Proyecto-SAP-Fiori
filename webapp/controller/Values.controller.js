0a6738f5afcb996ac126bd2b76e9c7a889a50929
25carlosalberto <scorpionmkx25@gmail.com>
2025-05-04 00:50:04 -0700

    View de values, solo el view

---
+++ webapp/controller/Values.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "com/invertions/sapfiorimodinv/controller/BaseController",
+  'sap/ui/model/json/JSONModel'
+], function (BaseController) {
+  "use strict";
+  
+    return BaseController.extend("com.ccnay.ficsapfioriwebsales.controller.Values", {
+      onInit() {},
+    });
+  });
