e974006a8dd2d58678007a7d2c78e4813aa40769
Yvan Acosta <acostacarrillo.yvan@gmail.com>
2025-05-19 02:37:48 -0700

    ajustes usuarios

--- webapp/controller/security/UsersList.controller.js
+++ webapp/controller/security/UsersList.controller.js
@@ -1,4 +1,5 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable valid-jsdoc */
 /* eslint-disable no-console */
 /* eslint-disable fiori-custom/sap-no-hardcoded-url */
 /* eslint-disable fiori-custom/sap-no-localhost */
@@ -7,141 +8,326 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/base/Log",
     "sap/ui/core/Fragment",
-    "sap/m/MessageToast"
-], function(BaseController,JSONModel,Log,Fragment,MessageToast){
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
+], function(BaseController, JSONModel, Log, Fragment, MessageToast, MessageBox) {
     "use strict";
 
-    return BaseController.extend("com.invertions.sapfiorimodinv.controller.security.UsersList",{
-        onInit: function(){
-            const oRolesModel = new JSONModel();
-            this.getView().setModel(oRolesModel, "users");
+    return BaseController.extend("com.invertions.sapfiorimodinv.controller.security.UsersList", {
+        onInit: function() {
+            // Modelo para controlar el estado de los botones (habilitados o no)
+            var oViewModel = new JSONModel({
+                buttonsEnabled: false
+            });
+            this.getView().setModel(oViewModel, "viewModel");
 
-            const oSelectedUserModel = new JSONModel();
+            // Crea un modelo para los usuarios y lo asigna a la vista (aquí se guarda la data en la propiedad "value")
+            var oUsersModel = new JSONModel();
+            this.getView().setModel(oUsersModel, "users");
 
-            this.getView().setModel(oSelectedUserModel, "selectedUser");
-
-            this._loadUsersData();
+            // Cargar los usuarios al iniciar la vista
+            this.loadUsers();
         },
 
-        _loadUsersData: async function(){
-            try {
-                const response = await fetch("http://localhost:4004/api/sec/usersCRUD?procedure=getall",{
-                    method:"POST",
-                    headers: {
-                        "Content-Type": "application/json" // Indica que se envían datos en JSON
+        /**
+         * Función para cargar la lista de usuarios desde la API.
+         */
+        loadUsers: function() {
+            var oTable = this.byId("IdTable1UsersManageTable");
+            var oModel = new JSONModel();
+            var that = this;
+
+            fetch("env.json")
+                .then(res => res.json())
+                .then(env => fetch(env.API_USERS_URL_BASE + "getallusers"))
+                .then(res => res.json())
+                .then(data => {
+                    // Formatear el campo ROLES para cada usuario antes de asignarlo al modelo
+                    data.value.forEach(user => {
+                        user.ROLES = that.formatRoles(user.ROLES);
+                    });
+                    oModel.setData(data);
+                    // Asigna el modelo a la tabla
+                    oTable.setModel(oModel);
+                })
+                .catch(err => {
+                    // Si ocurre un error con setModel, se ignora; en otro caso se muestra el mensaje
+                    if (err.message === "Cannot read properties of undefined (reading 'setModel')") {
+                        return;
+                    } else {
+                        MessageToast.show("Error al cargar usuarios: " + err.message);
                     }
                 });
-                const data = await response.json();
-                //console.log("ESTO ES LA RESPUESTA:",data);
-                // Guardamos todo el array de usuarios
-                this.getView().getModel("users").setData({ value: data.value });
-              } catch (error) {
-                Log.error("Error al cargar Usuarios", error);
-              }
         },
-        
-        onListItemPressed:function(oEvent){
-            const oListItem = oEvent.getParameter("listItem");
-            const oContext = oListItem.getBindingContext("users");
-            
-            if (!oContext) {
-                console.error("No se encontró el contexto del rol seleccionado.");
+
+        loadCompanies: function() {
+            // Agregar la lógica para cargar compañías, según sea necesario
+        },
+
+        loadDeptos: function() {
+            // Agregar la lógica para cargar departamentos según la compañía
+        },
+
+        loadRoles: function() {
+            var oView = this.getView();
+            var oRolesModel = new JSONModel();
+
+            fetch("env.json")
+                .then(res => res.json())
+                .then(env => fetch(env.API_ROLES_URL_BASE + "getallroles"))
+                .then(res => res.json())
+                .then(data => {
+                    oRolesModel.setData({ roles: data.value });
+                    // Asigna el modelo de roles a la vista (puedes usarlo en la lista de roles de la modal)
+                    oView.setModel(oRolesModel);
+                })
+                .catch(err => MessageToast.show("Error al cargar roles: " + err.message));
+        },
+
+        formatRoles: function(rolesArray) {
+            return Array.isArray(rolesArray)
+                ? rolesArray.map(role => role.ROLENAME).join("-")
+                : "";
+        },
+
+       
+        onRoleSelected: function(oEvent) {
+            var oComboBox = oEvent.getSource();
+            var sSelectedKey = oComboBox.getSelectedKey();
+            var sSelectedText = oComboBox.getSelectedItem().getText();
+
+            var oVBox;
+            // Determina si es la modal de "add user" o "edit user"
+            if (oComboBox.getId().includes("comboBoxEditRoles")) {
+                oVBox = this.getView().byId("selectedEditRolesVBox");
+            } else {
+                oVBox = this.getView().byId("selectedRolesVBox");
+            }
+
+            // Verificar si el rol ya está agregado
+            var bExists = oVBox.getItems().some(oItem => oItem.data("roleId") === sSelectedKey);
+            if (bExists) {
+                MessageToast.show("El rol ya ha sido añadido.");
                 return;
             }
 
-            const oSelectedUser = oContext.getObject();
-            
-            const oSelectedUserModel = new JSONModel(oSelectedUser);
-            this.getOwnerComponent().setModel(oSelectedUserModel, "selectedUser");
-            
-            //✅ Navegar al detalle
-            this.getOwnerComponent().getRouter().navTo("RouteUserDetails", {
-                USERID: encodeURIComponent(oSelectedUser.USERID)
+            // Crear un HBox con el rol seleccionado y un botón para eliminarlo
+            var oHBox = new sap.m.HBox({
+                items: [
+                    new sap.m.Label({ text: sSelectedText }).addStyleClass("sapUiSmallMarginEnd"),
+                    new sap.m.Button({
+                        icon: "sap-icon://decline",
+                        type: "Transparent",
+                        press: () => oVBox.removeItem(oHBox)
+                    })
+                ]
             });
+            oHBox.data("roleId", sSelectedKey);
+            oVBox.addItem(oHBox);
         },
 
-        onAddUser : function() {
-            if (!this._oUserDialog) {
+        /**
+         * Abre la ventana de diálogo para agregar un nuevo usuario.
+         */
+        onAddUser: function() {
+            var oView = this.getView();
+
+            if (!this._oCreateUserDialog) {
                 Fragment.load({
-                    id: this.getView().getId(),
-                    name: "com.invertions.sapfiorimodinv.view.security.UserDialog",
+                    id: oView.getId(),
+                    name: "com.invertions.sapfiorimodinv.view.security.fragments.AddUserDialog",
                     controller: this
-                }).then( (oDialog) => {
-                    this.getView().addDependent(oDialog);
-                    this._oUserDialog = oDialog;
-                    // Crea y asigna un modelo local para el diálogo
-                    var oDialogModel = new JSONModel({
-                        USERID:"",
-                        USERNAME:"",
-                        FIRSTNAME: "",
-                        LASTNAME: "",
-                        ALIAS: "",
-                        EMAIL: "",
-                        BIRTHDAYDATE: "",
-                        DEPARTMENT: "",
-                        FUNCTION: "",
-                        STREET: "",
-                        CITY: "",
-                        STATE: "",
-                        POSTALCODE: "",
-                        PHONENUMBER: "",
-                        COUNTRY: "",
-                        ROLES: ""
-                    });
-                    oDialogModel.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
-                    oDialog.setModel(oDialogModel, "UserDialogModel");
+                }).then(oDialog => {
+                    this._oCreateUserDialog = oDialog;
+                    oView.addDependent(oDialog);
+                    this.loadRoles();
                     oDialog.open();
                 });
             } else {
-                // Si el diálogo ya existe, reinicia el modelo
-                //var oModel = this._oUserDialog.getModel("UserDialogModel");
-                this._oUserDialog.open();
+                this._oCreateUserDialog.open();
             }
         },
 
-        onCancelUser: function(){
-            if (this._oUserDialog) {
-                this._oUserDialog.close();
+        /**
+         * Lógica al presionar "Guardar" en el diálogo de Agregar Usuario.
+         * Aquí se debería enviar la información al backend.
+         */
+        onSaveUser: function() {
+            // Aquí la lógica para agregar el usuario. Por ejemplo, recoger los datos del modelo "UserDialogModel"
+            // y enviarlos mediante fetch.
+        },
+
+        onCancelUser: function() {
+            if (this._oCreateUserDialog) {
+                this._oCreateUserDialog.close();
             }
         },
-        onSaveUser: async function() {
-            var oDialogModel = this._oUserDialog.getModel("UserDialogModel");
-            var oNewUser = oDialogModel.getData();
-            console.log("Datos guardados en el modelo local:",oNewUser);
-            // Validar datos básicos, por ejemplo:
-            if (!oNewUser.FIRSTNAME || !oNewUser.LASTNAME || !oNewUser.EMAIL) {
-                MessageToast.show("Por favor, complete los campos obligatorios.");
+
+        /**
+         * Abre la ventana de diálogo para editar el usuario seleccionado.
+         */
+        onEditUser: function() {
+            var oView = this.getView();
+
+            if (!this._oEditUserDialog) {
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "com.invertions.sapfiorimodinv.view.security.fragments.EditUserDialog",
+                    controller: this
+                }).then(oDialog => {
+                    this._oEditUserDialog = oDialog;
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                });
+            } else {
+                this._oEditUserDialog.open();
+            }
+        },
+
+        /**
+         * Lógica para guardar la información actualizada del usuario.
+         */
+        onEditSaveUser: function() {
+            // Aquí la lógica para actualizar el usuario en la base de datos.
+        },
+
+        onEditCancelUser: function() {
+            if (this._oEditUserDialog) {
+                this._oEditUserDialog.close();
+            }
+        },
+
+        /**
+         * Elimina el usuario seleccionado, previa confirmación.
+         */
+        onDeleteUser: function() {
+            if (this.selectedUser) {
+                var that = this;
+                MessageBox.confirm("¿Deseas eliminar el usuario con nombre: " + this.selectedUser.USERNAME + "?", {
+                    title: "Confirmar eliminación",
+                    icon: MessageBox.Icon.WARNING,
+                    onClose: function(oAction) {
+                        if (oAction === MessageBox.Action.OK) {
+                            that.deleteUser(that.selectedUser.USERID);
+                        }
+                    }
+                });
+            } else {
+                MessageToast.show("Selecciona un usuario para eliminar de la base de datos");
+            }
+        },
+
+        /**
+         * Función para eliminar al usuario; aquí iría la llamada al backend.
+         */
+        deleteUser: function(UserId) {
+            // Aquí agregar la lógica para eliminar el usuario de la BD.
+        },
+
+        /**
+         * Desactiva el usuario seleccionado, previa confirmación.
+         */
+        onDesactivateUser: function() {
+            if (this.selectedUser) {
+                var that = this;
+                MessageBox.confirm("¿Deseas desactivar el usuario con nombre: " + this.selectedUser.USERNAME + "?", {
+                    title: "Confirmar desactivación",
+                    icon: MessageBox.Icon.WARNING,
+                    onClose: function(oAction) {
+                        if (oAction === MessageBox.Action.OK) {
+                            that.desactivateUser(that.selectedUser.USERID);
+                        }
+                    }
+                });
+            } else {
+                MessageToast.show("Selecciona un usuario para desactivar");
+            }
+        },
+
+        desactivateUser: function(UserId) {
+            // Aquí agregar la lógica para desactivar al usuario
+        },
+
+        /**
+         * Activa el usuario seleccionado, previa confirmación.
+         */
+        onActivateUser: function() {
+            if (this.selectedUser) {
+                var that = this;
+                MessageBox.confirm("¿Deseas activar el usuario con nombre: " + this.selectedUser.USERNAME + "?", {
+                    title: "Confirmar activación",
+                    icon: MessageBox.Icon.WARNING,
+                    onClose: function(oAction) {
+                        if (oAction === MessageBox.Action.OK) {
+                            that.activateUser(that.selectedUser.USERID);
+                        }
+                    }
+                });
+            } else {
+                MessageToast.show("Selecciona un usuario para activar");
+            }
+        },
+
+        activateUser: function(UserId) {
+            // Aquí agregar la lógica para activar al usuario en la BD.
+        },
+
+        /**
+         * Se ejecuta cuando se selecciona una fila en la tabla.
+         * Actualiza el modelo "viewModel" para habilitar los botones y asigna el usuario seleccionado.
+         */
+        onUserRowSelected: function() {
+            var oTable = this.byId("IdTable1UsersManageTable");
+            var iSelectedIndex = oTable.getSelectedIndex();
+
+            if (iSelectedIndex < 0) {
+                this.getView().getModel("viewModel").setProperty("/buttonsEnabled", false);
                 return;
             }
-            oNewUser.USERNAME = oNewUser.FIRSTNAME + " " + oNewUser.LASTNAME;
 
-            // Enviar datos a la API usando fetch con configuración adecuada:
-            try {
-                // Enviar la solicitud POST incluyendo
-                const response = await fetch("http://localhost:4004/api/sec/usersCRUD?procedure=post", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(oNewUser)
-                });
-                console.log(response);
-                // Puedes verificar el estado de la respuesta o procesar la respuesta:
-                if (response.ok) {
-                    MessageToast.show("El usuario se ha guardado correctamente.");
-                } else {
-                    MessageToast.show("Error al guardar el usuario en la base de datos.");
-                }
+            var oContext = oTable.getContextByIndex(iSelectedIndex);
+            var UserData = oContext.getObject();
+            this.selectedUser = UserData;
 
-                
-            } catch (error) {
-                console.error(error);
-                MessageToast.show("Error de conexión a la API.");
+            // Activa los botones
+            this.getView().getModel("viewModel").setProperty("/buttonsEnabled", true);
+        },
+
+        /**
+         * Aplica un filtro de búsqueda a la tabla de usuarios.
+         */
+        onSearchUser: function(oEvent) {
+            var sQuery = oEvent.getParameter("query");
+            var oTable = this.byId("IdTable1UsersManageTable");
+            var oBinding = oTable.getBinding("rows");
+
+            if (sQuery && sQuery.length > 0) {
+                var aFilters = [
+                    new sap.ui.model.Filter("USERNAME", sap.ui.model.FilterOperator.Contains, sQuery)
+                ];
+                oBinding.filter(aFilters);
+            } else {
+                oBinding.filter([]);
             }
-            
-            this._oUserDialog.close();
-            MessageToast.show("El usuario se ha guardado correctamente.");
-            //this._loadUsersData();
+        },
+
+        onRefresh: function() {
+            this.loadUsers();
+        },
+
+        /**
+         * Validación simple para correos electrónicos.
+         */
+        isValidEmail: function(email) {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        },
+
+        /**
+         * Validación simple para números telefónicos (10 dígitos).
+         */
+        isValidPhoneNumber: function(phone) {
+            return /^\d{10}$/.test(phone);
         }
     });
 });
