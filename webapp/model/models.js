8d6513c8c3828a3139e930f64d773ed6ac980c30
Yvan Acosta <acostacarrillo.yvan@gmail.com>
2025-05-26 00:19:23 -0700

    Creo que es todo, aver si no le di en su pta maddre

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,15 +1,39 @@
-// models.js
 sap.ui.define([
-  "sap/ui/model/json/JSONModel",
-  "sap/ui/Device"
-], function (JSONModel, Device) {
-  "use strict";
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/Device"
+],
+    function (JSONModel, Device) {
+        "use strict";
 
-  return {
-    createDeviceModel: function () {
-      const oModel = new JSONModel(Device);
-      oModel.setDefaultBindingMode("OneWay");
-      return oModel;
-    }
-  };
-});
+        return {
+            /**
+             * Provides runtime information for the device the UI5 app is running on as a JSONModel.
+             * @returns {sap.ui.model.json.JSONModel} The device model.
+             */
+            createDeviceModel: function () {
+                var oModel = new JSONModel(Device);
+                oModel.setDefaultBindingMode("OneWay");
+                return oModel;
+            },
+
+            createRolesModel: function () {
+                return new JSONModel({ value: [] }); // Para la lista de roles
+            },
+
+            createSelectedRoleModel: function () {
+                return new JSONModel({}); // Para el rol seleccionado
+            },
+
+            createNewRoleModel: function () {
+                return new JSONModel({
+                    ROLEID: "",
+                    ROLENAME: "",
+                    DESCRIPTION: "",
+                    NEW_PROCESSID: "",
+                    NEW_PRIVILEGES: [],
+                    PRIVILEGES: []
+                });
+            }
+        };
+
+    });
